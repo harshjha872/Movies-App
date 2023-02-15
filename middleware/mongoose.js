5272bbee4d8e605e7f822144a85016337cb801ad
Harsh Jha <harshjha872@gmail.com>
2023-02-15 19:02:41 +0530

    changed uri

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -4,7 +4,7 @@ const connectdb = (handler) => async (req, res) => {
   if (await mongoose.connections[0].readyState) {
     return handler(req, res);
   }
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(process.env.MONGO_URI);  //changed uri
   return handler(req, res);
 };
 
